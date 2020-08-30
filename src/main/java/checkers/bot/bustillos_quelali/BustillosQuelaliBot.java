6e4924611ac846a2cb8b95a71d49ee88b15ce9a3
AlejandraDay <alejandra.day.123@gmail.com>
2020-08-29 22:56:04 -0400

    Add check terminal state.

--- src/main/java/checkers/bot/bustillos_quelali/BustillosQuelaliBot.java
+++ src/main/java/checkers/bot/bustillos_quelali/BustillosQuelaliBot.java
@@ -1,4 +1,32 @@
 package checkers.bot.bustillos_quelali;
 
-public class BustillosQuelaliBot {
+import checkers.CheckersBoard;
+import checkers.CheckersMove;
+import checkers.CheckersPlayer;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class BustillosQuelaliBot implements CheckersPlayer{
+
+    public boolean checkTerminalState(CheckersBoard board) {
+        List<CheckersMove> possibleCaptures = board.possibleCaptures();
+        CheckersBoard.Player otherPlayer = board.otherPlayer(); //del oponente
+        if (possibleCaptures.isEmpty()){
+            return true;
+        } else if(!board.isMovePossible() && !board.isCapturePossible()){ //current player
+            board.switchTurn();
+            if(!board.isMovePossible() && !board.isCapturePossible()){ // opponent player
+                board.switchTurn();
+                return true;//if the opponent doesn't have more pieces or if is a tie.
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public CheckersMove play(CheckersBoard board) {
+       return null;
+    }
+
 }
